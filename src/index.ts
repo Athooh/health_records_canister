450fc1ab4cf18f51d3f9a01c7c6fb2c4338b144c
Rutarenzi <rutagaramaaxcel@gmail.com>
2025-01-04 23:01:58 +0200

    improve the code base

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 // Importing required dependencies
 import { v4 as uuidv4 } from "uuid";
-import { StableBTreeMap, ic } from "azle";
-import express from "express";
+import {  Server, StableBTreeMap, ic } from 'azle/experimental';
+import express, { Request, Response,NextFunction } from 'express';
 
 // Defining the structure of a health record
 class HealthRecord {
@@ -16,9 +16,7 @@ class HealthRecord {
 // Initializing the storage for health records
 const healthRecordsStorage = StableBTreeMap<string, HealthRecord>(0);
 
-// Setting up the Express application
-const app = express();
-app.use(express.json());
+
 
 // Helper function to get the current date
 function getCurrentDate() {
@@ -26,62 +24,93 @@ function getCurrentDate() {
     return new Date(timestamp.valueOf() / 1_000_000);
 }
 
-// Endpoint to create a new health record
-app.post("/health-records", (req, res) => {
-    const healthRecord: HealthRecord = {
-        id: uuidv4(),
-        createdAt: getCurrentDate(),
-        updatedAt: null,
-        ...req.body,
-    };
-    healthRecordsStorage.insert(healthRecord.id, healthRecord);
-    res.json(healthRecord);
+export default Server(()=>{
+  // Setting up the Express application
+     const app = express();
+     app.use(express.json());
+    
+     // Endpoint to create a new health record
+     app.post('/health-records', (req:any, res:Response) => {
+      try{
+
+        const healthRecord: HealthRecord = {
+            id: uuidv4(),
+            createdAt: getCurrentDate(),
+            updatedAt: null,
+            ...req.body,
+        };
+        healthRecordsStorage.insert(healthRecord.id, healthRecord)
+       return res.status(201).json({ status:201, healthRecord});
+      }catch(error: any) {
+        console.log(error)
+        return res.status(500).json({ status: 500, error: error.message })
+      }
 });
 
 // Endpoint to retrieve all health records
-app.get("/health-records", (req, res) => {
-    res.json(healthRecordsStorage.values());
+app.get("/health-records", (req:Request, res:Response) => {
+   try{
+    return res.status(200).json({ status:200, AllHealth:healthRecordsStorage.values()});
+   }catch(error: any) {
+    return res.status(500).json({status: 500, error: error.message})
+   }
 });
 
 // Endpoint to retrieve a specific health record by ID
-app.get("/health-records/:id", (req, res) => {
+app.get("/health-records/:id", (req:Request, res:Response) => {
+  try{
     const recordId = req.params.id;
     const recordOpt = healthRecordsStorage.get(recordId);
     if (!recordOpt) {
-        res.status(404).send(`Health record with id=${recordId} not found`);
+       return res.status(404).send(`Health record with id=${recordId} not found`);
     } else {
-        res.json(recordOpt.Some);
+       return res.status(200).json({status: 200, recordOpt});
     }
+  }catch(error: any) {
+    return res.status(500).json({status: 500, error: error.message})
+  }
 });
 
 // Endpoint to update a health record
-app.put("/health-records/:id", (req, res) => {
+app.put("/health-records/:id", (req:Request, res:Response) => {
+   try{
     const recordId = req.params.id;
     const recordOpt = healthRecordsStorage.get(recordId);
     if (!recordOpt) {
-        res.status(400).send(`Couldn't update health record with id=${recordId}. Record not found.`);
+       return res.status(400).send(`Couldn't update health record with id=${recordId}. Record not found.`);
     } else {
-        const record = recordOpt.Some!; // Add non-null assertion to indicate that record is defined
+        const record = recordOpt; // Add non-null assertion to indicate that record is defined
         const updatedRecord = {
             ...record,
             ...req.body,
             updatedAt: getCurrentDate(),
         };
         healthRecordsStorage.insert(record.id, updatedRecord);
-        res.json(updatedRecord);
+        return res.status(200).json({ status:200, updatedRecord });
     }
+   }catch(error: any) {
+    return res.status(500).json({ status: 500, error: error.message })
+   }
 });
 
 // Endpoint to delete a health record
-app.delete("/health-records/:id", (req, res) => {
+app.delete("/health-records/:id", (req:Request, res:Response) => {
+   try{
     const recordId = req.params.id;
     const deletedRecord = healthRecordsStorage.remove(recordId);
     if (!deletedRecord) {
-        res.status(400).send(`Couldn't delete health record with id=${recordId}. Record not found.`);
+       return res.status(400).send(`Couldn't delete health record with id=${recordId}. Record not found.`);
     } else {
-        res.json(deletedRecord.Some);
+      return res.status(200).json({ status:200, deletedRecord });
     }
+   }catch(error:any) {
+    return res.status(500).json({status: 500, error: error.message})
+   }
 });
 
 // Starting the server
-app.listen();
+const PORT = 8000
+return app.listen(PORT,()=>{
+    console.log(`Server is running on port ${PORT}`)
+})
+})
